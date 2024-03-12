f73fde88b4405bc3ac90c2672c61b25b562ffb92
Rizal Hidayat <rizalhidayat006@gmail.com>
2024-03-12 04:15:00 +0000

    add fungfionality datetime & character counting

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+const { format } = require('date-fns');
+const { size } = require('lodash');
+
+function formatDate(date, formatStr) {
+  return format(date, formatStr);
+}
+
+function countCharacters(str) {
+  return size(str);
+}
+
 function lowercase(str) {
-    return str.toLowerCase();
-  }
-  
-  function uppercase(str) {
-    return str.toUpperCase();
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  
-  module.exports = {
-    lowercase,
-    uppercase,
-    capitalize,
-  };
-  
+  return str.toLowerCase();
+}
+
+function uppercase(str) {
+  return str.toUpperCase();
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = {
+  lowercase,
+  uppercase,
+  capitalize,
+  formatDate,
+  countCharacters,
+};
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-const { lowercase, uppercase, capitalize } = require('./index');
+const { lowercase, uppercase, capitalize, formatDate, countCharacters } = require('./index');
 
 let text = 'hello, world!';
+let date = new Date();
 
 console.log(lowercase(text)); // Output: hello, world!
 console.log(uppercase(text)); // Output: HELLO, WORLD!
 console.log(capitalize(text)); // Output: Hello, world!
+console.log(formatDate(date, 'dd/MM/yyyy')); // Output: 09/03/2024
+console.log(countCharacters(text)); // Output: 13
